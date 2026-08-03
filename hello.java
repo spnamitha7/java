2f6da05d778cb8adf59588b95adadbca97211654
spnamitha7 <spnamitha7@gmail>
2026-08-03 16:27:01 +0530

    Primitive Type

---
+++ hello.java
@@ -0,0 +1,25 @@
+public class hello{
+    public static void main(String args[]){
+        boolean a=true;
+        char s='a';
+        byte f=23;
+        short l=994;
+        int k=439505555;
+        long g=435454l;
+        float p=3.34f;
+        double d=3.34;
+        System.out.println(a);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(k);
+        System.out.println(g);
+        System.out.println(p);
+        System.out.println(d);
+        String str=new String();
+        str="hello";
+        str.concat("world");
+        System.out.println(str + "hello");
+
+    }
+}
