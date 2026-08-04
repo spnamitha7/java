e96933dfb755888d5d35f04728c7a18d3db6b400
spnamitha7 <spnamitha7@gmail>
2026-08-04 15:50:17 +0530

    textblocks

---
+++ textblocks.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class textblocks{
+    public static void main(String[] args) {
+        String s="""
+                namitha
+                department of it
+                final year
+                b section
+                spnamitha7@gmail.com
+
+                """;
+                System.out.println(s);
+    }
+}
