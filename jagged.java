d55ed7b58b598e86e25b2059f78494b8bce999ba
spnamitha7 <spnamitha7@gmail>
2026-08-04 15:36:09 +0530

    Array

--- jagged.java
+++ jagged.java
@@ -20,5 +20,15 @@ public class jagged {
             }
         }
 System.out.println(sum);
+
+int lar=arr[0][0];
+for(int i=0;i<n;i++){
+    for(int j=0;j<arr[i].length;j++){
+        if(arr[i][j]>=lar){
+            lar=arr[i][j];
+        }
+    }
+}
+System.out.println(lar);
     }
 }
