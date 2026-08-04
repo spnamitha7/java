423113fc7d61809bf7736b29198619ff8a3ac31b
spnamitha7 <spnamitha7@gmail>
2026-08-04 15:53:39 +0530

    records

---
+++ records.java
@@ -0,0 +1,10 @@
+record book(int id,String author){
+
+}
+public class records {
+    public static void main(String[] args) {
+        book b=new book(101, "nelson");
+        System.out.println(b.id());
+        System.out.println(b.author());
+    }
+}
